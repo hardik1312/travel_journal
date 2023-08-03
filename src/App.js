101ab3c4c6eba0db420a0fb7265195aeab4224b6
Hardik Khatri <hardikkhatri1312@gmail.com>
2023-08-03 16:38:38 +0530

    Added structure and props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import Navbar from './components/Navbar'
 import Main from './components/Main'
+import data from './data'
+
 function App(){
+
+  const mainData = data.map(element=>{
+    return(
+      <Main 
+      key={element.id}
+      element={element}/>
+    )
+  })
   return(
     <div>
       <Navbar />
-      <Main />
+      {mainData}
     </div>
   )
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,16 @@
-function Main(){
+import Location from '../images/Location.png'
+
+function Main(props){
     return(
-        <h1>Main</h1>
+        <section>
+            <img src={Location} alt='location'/>
+            <img src={props.element.img} alt="fuji"></img>
+            <p>{props.element.country}</p>
+            <p>View on Google Maps</p>
+            <h1>{props.element.location}</h1>
+            <p>{props.element.date}</p>
+            <p>{props.element.description}</p>
+        </section>
     )
 }
 
